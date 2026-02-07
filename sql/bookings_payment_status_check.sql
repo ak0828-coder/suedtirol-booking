0bd8b8d79509df16e7890d7a54ff8bf0f9985faf
Alexander Kofler <alexander.kofler06@gmail.com>
2026-02-07 21:54:37 +0100

    Add SQL to fix bookings payment_status check

---
+++ sql/bookings_payment_status_check.sql
@@ -0,0 +1,7 @@
+-- Fix bookings_payment_status_check to allow unpaid for Stripe checkout
+ALTER TABLE public.bookings
+  DROP CONSTRAINT IF EXISTS bookings_payment_status_check;
+
+ALTER TABLE public.bookings
+  ADD CONSTRAINT bookings_payment_status_check
+  CHECK (payment_status IN ('unpaid','paid_stripe','paid_cash','paid_member'));
